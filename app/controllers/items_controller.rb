d1ded1c956edf493ecb5ee13a7db4fbd15101bfb
masaaki1983 <saikyou_sw@yahoo.co.jp>
2020-08-17 19:40:25 +0900

    users_controller作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,21 +1,3 @@
 class ItemsController < ApplicationController
-  def index
-  end
-
-  def edit
-  end
-
-  def update
-    if current_user.update(user_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:nickname, :email)
-  end
+  
 end
