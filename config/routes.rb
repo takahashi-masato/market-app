51937f4f820e2b8bebdb853568fa64cb12340f2a
masaaki1983 <saikyou_sw@yahoo.co.jp>
2020-08-17 19:55:43 +0900

    ルーティングをネスト

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
 
-  resources :users, only: [:edit, :updata]
-  resources :identifications, only: [:new, :create, :edit, :updata]
-  resources :deliver_addresses
+  resources :users, only: [:edit, :updata] do
+    resources :identifications, only: [:new, :create, :edit, :updata]
+    resources :deliver_addresses
+  end
 end
