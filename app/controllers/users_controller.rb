d2844559802833b3a7ad111bfedcfebcd1412154
masaaki1983 <saikyou_sw@yahoo.co.jp>
2020-08-18 22:01:04 +0900

    usercontroller変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(user_params)
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
@@ -17,5 +27,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:nickname, :email)
+    params.require(:identification).permit(:family_name_kanji, :first_name_kanji, :family_name_kana, :first_name_kana, :birthday)
   end
 end
