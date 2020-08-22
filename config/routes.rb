53a88f8e888c43b34fc02845cfb8a60b5feb8ae9
masaaki1983 <saikyou_sw@yahoo.co.jp>
2020-08-22 19:48:26 +0900

    add-devise_user_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'users#index'
+  devise_for :users, :controllers => {
+ :registrations => 'users/registrations'
+}
+
+devise_scope :user do
+  get 'users/idetification', to: "users/registrations#new_identification"
+  
+end
+
+  # root 'users#index'
   resources :users, only: [:index, :new, :create, :edit, :update, :show] do
     resources :identifications, only: [:index, :new, :create, :edit, :update]
     resources :deliver_addresses
