1bbb62bb9ea9b5aeedf47e75b43c909ca3bd0d57
masaaki1983 <saikyou_sw@yahoo.co.jp>
2020-08-22 15:45:52 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'items#index'
-
-  resources :users, only: [:index, :new, :create, :edit, :update] do
-    resources :identifications, only: [:new, :create, :edit, :update]
+  root 'users#index'
+  resources :users, only: [:index, :new, :create, :edit, :update, :show] do
+    resources :identifications, only: [:index, :new, :create, :edit, :update]
     resources :deliver_addresses
   end
 end
