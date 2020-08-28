6bcc19c37a80b51cb952d59065fcee0f7f1ecbdf
tomato-nit <65883963+tomato-nit@users.noreply.github.com>
2020-08-28 20:59:24 +0900

    出品機能マークアップ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,6 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    binding.pry
     @item.seller_id = current_user.id
     if @item.save
       redirect_to root_path
