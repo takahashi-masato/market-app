6fc52ef166c4ce18b4535bcac3b0cc29a9b53dbe
masaaki1983 <saikyou_sw@yahoo.co.jp>
2020-09-01 22:13:28 +0900

    Update sessions_controller.rb

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,20 +8,39 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
+  def new
+    @user = User.new
+  end
+
   # POST /resource/sign_in
   # def create
   #   super
   # end
+  def create
+    @user = User.new(configure_sign_in_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
+  def destroy
+  end
 
-  # protected
+  protected
+
+  # def user_params
+  #   params.require(:identifications).permit(:family_name_kanji, :first_name_kanji, :family_name_kana, :first_name_kana,
+  #                                           :birthday, :postal_code, :prefecture, :city, :address1, :address2, :telephone)
+  # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname])
+  end
 end
