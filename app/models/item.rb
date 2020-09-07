d1073113ceb6884c8068819300fe54a05901a102
tomato-nit <65883963+tomato-nit@users.noreply.github.com>
2020-09-07 19:11:49 +0900

    出品機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,6 @@ class Item < ApplicationRecord
   #sellerでクラスを指定。値を取り出す場合はここを使う。
   # accepts_nested_attributes_for :images
   # is_impressionable
+  validates :name,:description,:brand_name,:item_condition,:shipping_payer,:shipping_from_area,:shipping_duration,:price,:seller_id, presence: true
+  validates :image_ids, presence: { message: "画像を選択してください" }
 end
