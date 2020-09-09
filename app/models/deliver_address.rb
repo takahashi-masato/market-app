fce53162a237366d35501483f4c7c772b7a4601b
masaaki1983 <saikyou_sw@yahoo.co.jp>
2020-09-09 21:08:22 +0900

    Update deliver_address.rb

--- app/models/deliver_address.rb
+++ app/models/deliver_address.rb
@@ -1,3 +1,4 @@
 class DeliverAddress < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
+  validates :family_name_kanji, :first_name_kanji, :family_name_kana, :first_name_kana, :postal_code, :prefecture, :city, :address1, :address2, :telephone, presence: true
 end
