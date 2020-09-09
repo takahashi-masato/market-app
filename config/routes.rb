2faa1a8acf27f29629e1d1424a8c7d7c14faf26d
masaaki1983 <saikyou_sw@yahoo.co.jp>
2020-09-09 21:08:51 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
-    :sessions => 'users/sessions'
 }
 
   devise_scope :user do
-    get 'users/identification', to: "users/registrations#new_identification"
-    get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy"
+    get 'identifications', to: 'users/registrations#new_identification'
+    post 'identifications', to: 'users/registrations#create_identification'
   end
 
 
