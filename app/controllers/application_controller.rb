a45ef0e1b1e8de2f88755955094b0409c1d48d09
masaaki1983 <saikyou_sw@yahoo.co.jp>
2020-09-13 15:28:27 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-
+  protect_from_forgery with: :exception
   before_action :basic_auth, if: :production?
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :family_name_kanji, :first_name_kanji, :family_name_kana, :first_name_kana, :birthday])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :email, :password])
   end
 
