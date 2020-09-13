c5b531323acb424884e450b961831ed67972e18a
masaaki1983 <saikyou_sw@yahoo.co.jp>
2020-09-13 21:29:43 +0900

    emailのバリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,8 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true, uniqueness: true
 
+  VALID_EMAIL_REGEX = /\A([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*\z/
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
+
 
 end
