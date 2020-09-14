0b53641a99e8e7dd191532a39115117525004d9e
masaaki1983 <saikyou_sw@yahoo.co.jp>
2020-09-15 00:51:54 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,25 +10,25 @@ describe User do
     it "nicknameがない場合は登録ができないこと" do
       user = build(:user, nickname: "")
       user.valid?
-      expect(user.errors[:nickname]).to include("translation missing: ja.activerecord.errors.models.user.attributes.nickname.blank")
+      expect(user.errors[:nickname]).to include("を入力してください")
     end
 
     it 'emailがない場合は登録できないこと' do
       user = build(:user, email: "")
       user.valid?
-      expect(user.errors[:email]).to include("translation missing: ja.activerecord.errors.models.user.attributes.email.blank")
+      expect(user.errors[:email]).to include("を入力してください")
     end
 
     it 'パスワードがない場合は登録ができないこと' do
       user = build(:user, password: "")
       user.valid?
-      expect(user.errors[:password]).to include("translation missing: ja.activerecord.errors.models.user.attributes.password.blank")
+      expect(user.errors[:password]).to include("を入力してください")
     end
 
     it "passwordが存在してもpassword_confirmationがない場合は登録できないこと" do
       user = build(:user, password_confirmation: "")
       user.valid?
-      expect(user.errors[:password_confirmation]).to include("translation missing: ja.activerecord.errors.models.user.attributes.password_confirmation.confirmation")
+      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
     end
 
     it " passwordが7文字以上であれば登録できること " do
@@ -39,14 +39,14 @@ describe User do
     it " passwordが6文字以下であれば登録できないこと " do
       user = build(:user, password: "000000", password_confirmation: "000000")
       user.valid?
-      expect(user.errors[:password]).to include("translation missing: ja.activerecord.errors.models.user.attributes.password.too_short")
+      expect(user.errors[:password]).to include("は7文字以上で入力してください")
     end
 
     it "重複したemailが存在する場合登録できないこと" do
       user = create(:user)
       another_user = build(:user, email: user.email)
       another_user.valid?
-      expect(another_user.errors[:email]).to include("translation missing: ja.activerecord.errors.models.user.attributes.email.taken")
+      expect(another_user.errors[:email]).to include("はすでに存在します")
     end
 
   end
